c4075b48276393e561606ac14aeacc02698ba90f
StarK-0 <91716994+StarK-0@users.noreply.github.com>
2022-10-10 14:46:32 +0530

    3commit

--- Main.py
+++ Main.py
@@ -30,13 +30,13 @@ def merge_sort(myList) -> None:
             k += 1
 # For all the remaining values
         while i < len(left):
-            myList[k] = left[k]
+            myList[k] = left[i]
             i += 1
             k += 1
 
         
         while j < len(right):
-            myList[k]=right[j]
+            myList[k]=right[k]
             j += 1
             k += 1
 
